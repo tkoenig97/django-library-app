8bcd391aed760cdebd2c4a880960de5fa4c0aa16
Tyler <tylerskoenig@gmail.com>
2023-03-17 10:41:12 -0500

    Tweaked views

--- book_storage/book_app/views.py
+++ book_storage/book_app/views.py
@@ -1,32 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import *
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
 def home(request):
     book_info = []
-    books = Book.objects.prefetch_related('author')
+    books = Book.objects.prefetch_related('authors')
     for book in books:
         book_info.append({
-        'book' : book,
-        'authors' : book.authors.all()
+            'book': book,
+            'authors': book.authors.all()
         })
-        print(book_info)
-        
-        
-    return render(request, 'pages/books.html', {"books":book_info, "nested":True})
+    return render(request, 'pages/books.html', {"books":book_info})
 
 def books_by_genre(request, id):
     pass
 
 def books_by_author(request, id):
     author = Author.objects.get(id = id)
+    
     data = {
         'books' : Author.objects.get(id = id).book.all(),
-        'nested': False
+        'title': author
     }
     
-    return render(request, 'pages/books.html', data)
+    return render(request, 'pages/specific.html', data)
+
 
 def book(request, id):
     pass
