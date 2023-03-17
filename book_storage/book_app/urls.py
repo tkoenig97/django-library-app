4a9b9b0d273d5aa156d26040bc5c4fa9835fd59c
Tyler <tylerskoenig@gmail.com>
2023-03-17 16:22:53 -0500

    Updated urls

--- book_storage/book_app/urls.py
+++ book_storage/book_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('genre/<int:id>/', views.books_by_genre),
     path('author/<int:id>/', views.books_by_author),
     path('book/<int:id>/', views.book),
+    path('new_book/', views.new_book)
 ]
