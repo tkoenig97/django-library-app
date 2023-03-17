c81790f3eb7f32faf639f3fa594f5c14aa4a64cb
Tyler <tylerskoenig@gmail.com>
2023-03-17 09:58:24 -0500

    Added models and fixtures

--- book_storage/book_app/models.py
+++ book_storage/book_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return (f"{self.first_name} {self.last_name}")
+    
+class Genre(models.Model):
+    title = models.CharField(max_length=250)
+    
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    quantity = models.IntegerField(default=1)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    authors = models.ForeignKey(Author, related_name="book")
